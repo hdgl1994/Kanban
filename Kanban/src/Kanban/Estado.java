beeca0d7bc1d70f1e6a2d334540fb9e88b79a231
Héctor <hdgl1994@gmail.com>
2014-03-05 08:18:01 -0600

    Versión 0.1

---
+++ Kanban/src/Kanban/Estado.java
@@ -0,0 +1,7 @@
+package Kanban;
+
+public enum Estado {
+	DO_TO,
+	IN_PROGRESS,
+	DONE
+}
---
+++ Kanban/src/Kanban/ListaDeTareas.java
@@ -0,0 +1,15 @@
+package Kanban;
+
+public class ListaDeTareas implements IDashboard{
+	private Tarea[] listaDeTareas;
+	
+	public boolean agregar(Tarea tarea)
+	{
+		return true;
+	}
+	public boolean borrar(Tarea tarea)
+	{
+		return true;
+	}
+
+}
